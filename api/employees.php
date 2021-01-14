5e76c610077a4529ba91987f9be563be15f24ee0
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2021-01-14 13:28:59 +0100

    Fixed minor mistakes in employees handler

--- api/employees.php
+++ api/employees.php
@@ -92,91 +92,90 @@
         }
 
         // Read arguments
-        $data = file_get_contents('php://input');
+        $data = urldecode(file_get_contents('php://input'));
         
-        if ( strpos($data, '=') ==  true ) {
-            $allPairs = array();
-            $data = explode('&', $data);
-            
-            foreach($data as $pair) {
-                $pair = explode('=', $pair);
-                $allPairs[$pair[0]] = $pair[1];
-            }
-            
-            // Check if there is any argument
-            if ( empty($allPairs) ) {
-                exit(json_encode(array('status' => 'failed', 'reason' => 'No arguments are set'), JSON_PRETTY_PRINT));
-            }
-            
-            $sqlQuery = "UPDATE employees SET ";
-
-            $array = array();
-
-            // Check if employeeID is set
-            if ( isset($allPairs['employeeID']) ) {
-                $newEmployeeID = $allPairs['employeeID'];
-                
-                // Check if the length of new employeeID is 4
-                if( strlen($newEmployeeID) != 4) {
-                    exit(json_encode(array('status' => 'failed', 'reason' => 'Invalid new employeeID length'), JSON_PRETTY_PRINT));
-                }
-
-                $sqlTest = $conn->query("SELECT COUNT(*) FROM employees where employeeID='$newEmployeeID'");
-                $testResult = $sqlTest->fetch_assoc();
-                
-                // Check if new employeeID is already used
-                if ( $testResult['COUNT(*)'] != 0 ) {
-                    exit(json_encode(array('status' => 'failed', 'reason' => 'This employeeID is already used'), JSON_PRETTY_PRINT));
-                }
-
-                array_push($array, "employeeID='$newEmployeeID'");
-            }
-
-            // Check if pin is set
-            if ( isset($allPairs['pin']) ) {
-                $newPin = $allPairs['pin'];
-
-                // Check if the length of new pin is 4
-                if( strlen($newPin) != 4) {
-                    exit(json_encode(array('status' => 'failed', 'reason' => 'Invalid new pin length'), JSON_PRETTY_PRINT));
-                }
-
-                array_push($array, "pin='$newPin'");
-            }
-
-            // Check if name is set
-            if ( isset($allPairs['name']) ) {
-                $newName = $allPairs['name'];
-                array_push($array, "name='$newName'");
-            }
-
-            // Check if surname is set
-            if ( isset($allPairs['surname']) ) {
-                $newSurname = $allPairs['surname'];
-                array_push($array, "surname='$newSurname'");
-            }
-
-            // Check if shift is set
-            if ( isset($allPairs['shift']) ) {
-                $newShift = $allPairs['shift'];
-
-                // Check if shift uses enum('dzienny', 'nocny')
-                if( strtolower($newShift) != 'dzienny' && strtolower($newShift) != 'nocny' ) {
-                    exit(json_encode(array('status' => 'failed', 'reason' => 'Invalid shift'), JSON_PRETTY_PRINT));
-                }
-
-                array_push($array, "shift='$newShift'");
-            }
-
-            $sqlQuery .= implode(', ', $array);
-
-            $sqlQuery .= " WHERE employeeID=$employeeID";
-
-            $sql = $conn->query($sqlQuery);
-
-            // Success
-            exit(json_encode(array('status' => 'success'), JSON_PRETTY_PRINT));
+        // Check if any argument is set
+        if ( !strpos($data, '=') ) {
+            exit(json_encode(array('status' => 'failed', 'reason' => 'No arguments are set'), JSON_PRETTY_PRINT));
         }
+
+        $allPairs = array();
+        $data = explode('&', $data);
+        
+        foreach($data as $pair) {
+            $pair = explode('=', $pair);
+            $allPairs[$pair[0]] = $pair[1];
+        }
+        
+        
+        $sqlQuery = "UPDATE employees SET ";
+
+        $array = array();
+
+        // Check if employeeID is set
+        if ( isset($allPairs['employeeID']) ) {
+            $newEmployeeID = $allPairs['employeeID'];
+            
+            // Check if the length of new employeeID is 4
+            if( strlen($newEmployeeID) != 4) {
+                exit(json_encode(array('status' => 'failed', 'reason' => 'Invalid new employeeID length'), JSON_PRETTY_PRINT));
+            }
+
+            $sqlTest = $conn->query("SELECT COUNT(*) FROM employees where employeeID='$newEmployeeID'");
+            $testResult = $sqlTest->fetch_assoc();
+            
+            // Check if new employeeID is already used
+            if ( $testResult['COUNT(*)'] != 0 ) {
+                exit(json_encode(array('status' => 'failed', 'reason' => 'This employeeID is already used'), JSON_PRETTY_PRINT));
+            }
+
+            array_push($array, "employeeID='$newEmployeeID'");
+        }
+
+        // Check if pin is set
+        if ( isset($allPairs['pin']) ) {
+            $newPin = $allPairs['pin'];
+
+            // Check if the length of new pin is 4
+            if( strlen($newPin) != 4) {
+                exit(json_encode(array('status' => 'failed', 'reason' => 'Invalid new pin length'), JSON_PRETTY_PRINT));
+            }
+
+            array_push($array, "pin='$newPin'");
+        }
+
+        // Check if name is set
+        if ( isset($allPairs['name']) ) {
+            $newName = $allPairs['name'];
+            array_push($array, "name='$newName'");
+        }
+
+        // Check if surname is set
+        if ( isset($allPairs['surname']) ) {
+            $newSurname = $allPairs['surname'];
+            array_push($array, "surname='$newSurname'");
+        }
+
+        // Check if shift is set
+        if ( isset($allPairs['shift']) ) {
+            $newShift = $allPairs['shift'];
+
+            // Check if shift uses enum('dzienny', 'nocny')
+            if( strtolower($newShift) != 'dzienny' && strtolower($newShift) != 'nocny' ) {
+                exit(json_encode(array('status' => 'failed', 'reason' => 'Invalid shift'), JSON_PRETTY_PRINT));
+            }
+
+            array_push($array, "shift='$newShift'");
+        }
+
+        $sqlQuery .= implode(', ', $array);
+
+        $sqlQuery .= " WHERE employeeID=$employeeID";
+
+        $sql = $conn->query($sqlQuery);
+
+        // Success
+        exit(json_encode(array('status' => 'success'), JSON_PRETTY_PRINT));
     }
     //DELETE
     else if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
