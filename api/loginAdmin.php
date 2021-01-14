679d722fc7bee60360ea315646d60b998ede4dc6
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2021-01-14 13:55:47 +0100

    Added loginEmployee handler (no auth yet)

---
+++ api/loginAdmin.php
@@ -0,0 +1,25 @@
+<?php
+    // Connection variable used to communicate with DB
+    $conn = new mysqli('localhost', 'root', '', 'rejestrator');
+
+    // GET
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        // GET /loginAdmin/username/password
+        $username = $conn->real_escape_string($_GET['username']);
+        $password = $conn->real_escape_string($_GET['password']);
+
+        $data = array();
+        $sql = $conn->query("SELECT COUNT(*)
+                                FROM administrators 
+                                WHERE username='$username'
+                                AND password='$password'");
+        
+        $result = $sql->fetch_assoc();
+
+        // Check if corrent combination on username and password
+        if ( $result['COUNT(*)'] == 0 ) {
+            exit(json_encode(array('status' => 'failed', 'reason' => 'Invalid username or password'), JSON_PRETTY_PRINT));
+        }
+        exit(json_encode(array('status' => 'success'), JSON_PRETTY_PRINT));
+    }
+?>
