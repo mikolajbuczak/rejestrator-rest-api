f02c0d03014469c404f565df2c1f521ff238cd6e
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2021-01-16 19:05:43 +0100

    Added canAddAdmin

---
+++ api/canAddAdmin.php
@@ -0,0 +1,67 @@
+<?php
+     // Connection variable used to communicate with DB
+     $conn = new mysqli('localhost', 'root', '', 'rejestrator');
+
+     if($_SERVER['REQUEST_METHOD'] != 'POST'){
+        http_response_code(400);
+        exit();
+     }
+
+    //POST
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if( isset($_POST['adminID']) && isset($_POST['username']) ) {
+
+            $adminID = $conn->real_escape_string($_POST['adminID']);
+            $username = $conn->real_escape_string($_POST['username']);
+
+            $test = $conn->query("SELECT COUNT(*) FROM administrators WHERE administratorID='$adminID' OR username='$username'");
+            $testResult = $test->fetch_assoc();
+        
+            // Check if employee exists
+            if ( $testResult['COUNT(*)'] != 0 ) {
+                http_response_code(404);
+                exit();
+            }
+
+            // Success
+            http_response_code(200);
+            exit();
+        }
+        else if( isset($_POST['adminID']) ) {
+            $adminID = $conn->real_escape_string($_POST['adminID']);
+
+            $test = $conn->query("SELECT COUNT(*) FROM administrators WHERE administratorID='$adminID'");
+            $testResult = $test->fetch_assoc();
+        
+            // Check if employee exists
+            if ( $testResult['COUNT(*)'] != 0 ) {
+                http_response_code(404);
+                exit();
+            }
+
+            // Success
+            http_response_code(200);
+            exit();
+        }
+        else if( isset($_POST['username']) ) {
+            $username = $conn->real_escape_string($_POST['username']);
+
+            $test = $conn->query("SELECT COUNT(*) FROM administrators WHERE username='$username'");
+            $testResult = $test->fetch_assoc();
+        
+            // Check if employee exists
+            if ( $testResult['COUNT(*)'] != 0 ) {
+                http_response_code(404);
+                exit();
+            }
+
+            // Success
+            http_response_code(200);
+            exit();
+        }
+        else {
+            http_response_code(400);
+            exit();
+        }
+    }
+?>
