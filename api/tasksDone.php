2beea54cb90e415e6725b3127101c35937d6811e
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2021-01-16 14:46:47 +0100

    Added tasksDone

---
+++ api/tasksDone.php
@@ -0,0 +1,114 @@
+<?php
+     // Connection variable used to communicate with DB
+     $conn = new mysqli('localhost', 'root', '', 'rejestrator');
+
+     // GET
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        // GET /tasksDome/employeeID
+        if(isset($_GET['employeeID'])) {
+            $id = $conn->real_escape_string($_GET['employeeID']);
+            $data = array();
+            $sql = $conn->query("SELECT *
+                                 FROM tasksdone 
+                                 WHERE employeeID='$id'
+                                 ORDER BY enddate DESC");
+            while($d = $sql->fetch_assoc()) {
+                $data[] = $d;
+            }
+        }
+        // GET /tasksDone/employeeID/endDate
+        else if(isset($_GET['employeeID'])) {
+            $id = $conn->real_escape_string($_GET['employeeID']);
+            $date = $conn->real_escape_string($_GET['endDate']);
+            $data = array();
+            $sql = $conn->query("SELECT *
+                                 FROM tasksdone 
+                                 WHERE employeeID='$id'
+                                 AND enddate='$date'");
+            while($d = $sql->fetch_assoc()) {
+                $data[] = $d;
+            }
+        }
+        // GET /done
+        else {
+            $data = array();
+            $sql = $conn->query("SELECT *
+                                 FROM tasksdone
+                                 ORDER BY enddate DESC");
+            while($d = $sql->fetch_assoc()) {
+                $data[] = $d;
+            }
+        }
+        http_response_code(200);
+        exit(json_encode($data, JSON_PRETTY_PRINT));
+    }
+    // POST
+    else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['employeeID']) && 
+            isset($_POST['task']) && 
+            isset($_POST['startdate']) && 
+            isset($_POST['enddate']) && 
+            isset($_POST['time'])) {
+
+            // Check if the url is correct
+            if ( isset($_GET['employeeID']) || isset($_GET['endDate']) ) {
+                http_response_code(404);
+                exit();
+            }
+            
+            // Get all arguments
+            $employeeID = $conn->real_escape_string($_POST['employeeID']);
+            $task = $conn->real_escape_string($_POST['task']);
+            $startDate = $conn->real_escape_string($_POST['startdate']);
+            $endDate = $conn->real_escape_string($_POST['enddate']);
+            $time = $conn->real_escape_string($_POST['time']);
+
+            if( !verifyDate($startDate) || !verifyDate($endDate)) {
+                http_response_code(404);
+                exit();
+            }
+
+            $sql = $conn->query("INSERT INTO tasksdone 
+                                 (employeeID, task, startdate, enddate, time)
+                                 VALUES
+                                 ('$employeeID', '$task', '$startDate', '$endDate', '$time')");
+
+            // Success
+            http_response_code(200);
+            exit();
+        }
+        else {
+            // Missing arguments
+            http_response_code(404);
+            exit();
+        }
+    }
+    //DELETE
+    else if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+        // Check if the employeeID is set
+        if ( !isset($_GET['employeeID']) ) {
+            http_response_code(404);
+            exit();
+        }
+
+        $employeeID = $conn->real_escape_string($_GET['employeeID']);
+
+        $conn->query("DELETE FROM tasksdone WHERE employeeID='$employeeID'");
+
+        // Success
+        http_response_code(200);
+        exit();
+    }
+
+    function verifyDate($date, $strict = true)
+    {
+        $dateTime = DateTime::createFromFormat('d.m.Y H:i', $date);
+        if ($strict) {
+            $errors = DateTime::getLastErrors();
+            if (!empty($errors['warning_count'])) {
+                return false;
+            }
+        }
+        return $dateTime !== false;
+    }
+?>
