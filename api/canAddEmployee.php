6a51981262af6ec4ef9c987ac722e72801587407
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2021-01-16 19:14:26 +0100

    Added canAddEmployees

---
+++ api/canAddEmployee.php
@@ -0,0 +1,33 @@
+<?php
+     // Connection variable used to communicate with DB
+     $conn = new mysqli('localhost', 'root', '', 'rejestrator');
+
+     if($_SERVER['REQUEST_METHOD'] != 'POST'){
+        http_response_code(400);
+        exit();
+     }
+
+    //POST
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if( isset($_POST['employeeID']) ) {
+            $employeeID = $conn->real_escape_string($_POST['employeeID']);
+
+            $test = $conn->query("SELECT COUNT(*) FROM employees WHERE employeeID='$employeeID'");
+            $testResult = $test->fetch_assoc();
+        
+            // Check if employee exists
+            if ( $testResult['COUNT(*)'] != 0 ) {
+                http_response_code(404);
+                exit();
+            }
+
+            // Success
+            http_response_code(200);
+            exit();
+        }
+        else {
+            http_response_code(400);
+            exit();
+        }
+    }
+?>
