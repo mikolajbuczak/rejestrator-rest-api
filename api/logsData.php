ef97fcc23721c6f00531fa98fc6e8dd35f7b77f2
Mikolaj Buczak <mikolaj.buczak@gmail.com>
2021-02-03 15:03:24 +0100

    Adds possibility of getting logsData of specific employee

--- api/logsData.php
+++ api/logsData.php
@@ -1,21 +1,36 @@
 <?php
-     // Connection variable used to communicate with DB
-     $conn = new mysqli('localhost', 'root', '', 'rejestrator');
+    // Connection variable used to communicate with DB
+    $conn = new mysqli('localhost', 'root', '', 'rejestrator');
 
-     if($_SERVER['REQUEST_METHOD'] != 'GET'){
+    if ($_SERVER['REQUEST_METHOD'] != 'GET'){
         http_response_code(400);
         $conn->close();
         exit();
-     }
+    }
 
-    $data = array();
-    $sql = $conn->query("SELECT employees.employeeID, employees.name, employees.surname, logs.date 
-                            FROM employees
-                            JOIN logs
-                            ON logs.employeeID=employees.employeeID
-                            ORDER BY str_to_date(logs.date, '%d.%m.%y %H:%i') DESC");
-    while($d = $sql->fetch_assoc()) {
-        $data[] = $d;
+    if (isset($_GET['employeeID'])) {
+        $id = $conn->real_escape_string($_GET['employeeID']);
+        $data = array();
+        $sql = $conn->query("SELECT employees.employeeID, employees.name, employees.surname, logs.date 
+                                FROM employees
+                                JOIN logs
+                                ON logs.employeeID=employees.employeeID
+                                WHERE logs.employeeID='$id'
+                                ORDER BY str_to_date(logs.date, '%d.%m.%y %H:%i') DESC");
+        while($d = $sql->fetch_assoc()) {
+            $data[] = $d;
+        }
+    }
+    else {
+        $data = array();
+        $sql = $conn->query("SELECT employees.employeeID, employees.name, employees.surname, logs.date 
+                                FROM employees
+                                JOIN logs
+                                ON logs.employeeID=employees.employeeID
+                                ORDER BY str_to_date(logs.date, '%d.%m.%y %H:%i') DESC");
+        while($d = $sql->fetch_assoc()) {
+            $data[] = $d;
+        }
     }
     http_response_code(200);
     $conn->close();
